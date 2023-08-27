a2ae2a185f237cddc99479c0a50fce212be2c871
Ben Vincent <ben@unkin.net>
2023-08-27 18:01:13 +1000

    Added docked laptop setup

---
+++ .local/bin/screen-dock.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 2560x1440 --pos 0x615 --rotate normal --output DisplayPort-3 --mode 2560x1440 --pos 2560x0 --rotate left --output DisplayPort-4 --off
