2d6bcd9ff792b77d0a8676b2bf2b7bfe0d291c3f
Ben Vincent <ben@unkin.net>
2023-08-27 19:13:27 +1000

    Added comamnds to enable/disable screen saver
      - change to laptop and enable screensaver
      - change to dock and disable screenaver

--- .local/bin/screen-laptop.sh
+++ .local/bin/screen-laptop.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off
+xset s 360 360 +dpms
+notify-send -t 3000 "Screensaver Enabled!"
